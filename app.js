564f14cb07876584ad71e511a978cba6c6e7a96f
arifwidiyatmiko <arifwidiyatmiko@gmail.com>
2018-09-20 10:20:54 +0700

    setup bootstrap & example page

--- app.js
+++ app.js
@@ -1,17 +1,17 @@
-const images = ['fox1','fox2','fox3','fox4'];
-const imgElem = document.querySelector('img');
+// const images = ['fox1','fox2','fox3','fox4'];
+// const imgElem = document.querySelector('img');
 
-function randomValueFromArray(array) {
-  let randomNo =  Math.floor(Math.random() * array.length);
-  return array[randomNo];
-}
+// function randomValueFromArray(array) {
+//   let randomNo =  Math.floor(Math.random() * array.length);
+//   return array[randomNo];
+// }
 
-setInterval(function() {
-  let randomChoice = randomValueFromArray(images);
-  imgElem.src = 'images/' + randomChoice + '.jpg';
-}, 2000)
+// setInterval(function() {
+//   let randomChoice = randomValueFromArray(images);
+//   imgElem.src = 'images/' + randomChoice + '.jpg';
+// }, 2000)
 
-// Register service worker to control making site work offline
+// // Register service worker to control making site work offline
 
 if('serviceWorker' in navigator) {
   navigator.serviceWorker
